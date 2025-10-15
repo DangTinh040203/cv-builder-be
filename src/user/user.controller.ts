bfe639d6474d30e6bff7a7e6ef3d37eac17c9af9
DangTinh42 <caodangtinh04022003@gmail.com>
2025-10-16 01:07:18 +0700

    add: get user endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 
 import { UserService } from '@/user/user.service';
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
+import { type Request } from 'express';
 
+@UseGuards(JwtAuthGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('profile')
+  async getUserProfile(@Req() req: Request) {
+    return this.userService.findById(req.user!._id);
+  }
 }
